4718ffc14f5e37466c32ca5b682004cb6c3c541a
Davranbek <davranbek_1995@mail.ru>
2023-07-02 01:25:31 +0500

    added a new page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
 
+
+////////////////////  Render dates for html page
+
 let cards = document.querySelector('.countries-cards');
+
 
 
 function renderData(data){
@@ -31,7 +35,9 @@ function renderData(data){
 
 renderData(data);
 
+//////////////////////////////////////////////////////////////
 
+//////////////////// Select option for filter countries by name;
 
 const names = [];
 const select = document.querySelector('.countries-search__select');
@@ -55,3 +61,56 @@ select.addEventListener('change', (e) => {
     renderData(result);
 })
 
+/////////////////////////////////////////////////////////////////////////
+
+let country = document.querySelectorAll('.countries-card');
+let countryItem = document.querySelector('.country-item');
+
+
+country.forEach((value) => {
+    value.addEventListener('click', (e) => {
+        window.location.href = "country.html";
+
+        data.forEach((value) => {
+        let card = document.createElement('div');
+        card.classList.add('country-card');
+            card.textContent = 
+            countryItem.append(card);
+        })
+    })
+})
+// country.addEventListener('click', (e) => {
+//     window.location.href = "country.html"
+//     console.log(e.target)
+//     // data.forEach((value) => {
+//     //     let card = document.createElement('div');
+//     //     card.classList.add('country-card');
+
+//     //     card.innerHTML = `
+//     //         <img class="country-card__img" src="${value.flag}" alt="flag">
+//     //         <div class="country-card__body">
+//     //             <h5 class="country-card__body--name">Belgium</h5>
+//     //             <div class="country-card__body--info">
+//     //                 <ul>
+//     //                     <li> Native Name: <span>België</span></li>
+//     //                     <li> Population:  <span>11,319,511</span></li>
+//     //                     <li> Region: <span>Europe</span></li>
+//     //                     <li> Sub Region: <span>Western Europe</span></li>
+//     //                     <li> Capital: <span>Brussels</span></li>
+//     //                 </ul>
+//     //                 <ul>
+//     //                     <li>Top Level Domain: <span>.be</span></li>
+//     //                     <li>Currencies: <span>Euro</span></li>
+//     //                     <li>Languages: <span>Dutch, French, German</span></li>
+//     //                 </ul>
+//     //             </div>
+
+//     //             <div class="country-card__body--border">
+//     //                 <p>Border Countries: </p> <span>France</span> <span>Germany</span> <span>Netherlands</span>
+//     //             </div>
+//     //         </div>       
+//     //     `
+
+//     //     countryItem.append(card);
+//     // })
+// })
